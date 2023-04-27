7ef9a977874a2de11319e1a87c9d1424ed40eb1d
Casana E. Bey <info@expresslayouts.com>
2023-04-26 22:57:36 -0400

    Add Pipelines

--- src/overlays/templates/PTI/ShowChatPTI/ShowChatPTI.style.ts
+++ src/overlays/templates/PTI/ShowChatPTI/ShowChatPTI.style.ts
@@ -1,5 +1,4 @@
 import styled from "styled-components";
-import bg from "../../../../assets/images/1520x20-desc-gradient.png";
 
 export const ShowChatWrapper = styled.div`
   position: absolute;
@@ -10,64 +9,5 @@ export const ShowChatWrapper = styled.div`
   height: 120px;
   width: 1520px;
   overflow: hidden;
-
   padding: 10px 20px;
-
-  background-size: cover;
-  background-color: #a15004;
-  background-image: url(${bg});
-`;
-
-export const EntireChatWrapper = styled.div`
-  width: 100%;
-  height: 100%;
-  /* background-color: #24082e; */
-  overflow: hidden;
-  padding: 10px;
-
-  display: grid;
-  grid-template-columns: auto 1fr;
-  gap: 0.625rem;
-
-  border-bottom: 3px solid #a0941c;
-  transition: left 500ms ease-in-out;
-`;
-
-export const ShowChatImage = styled.div`
-  width: 65px;
-  height: 65px;
-  background-color: #24082e;
-  overflow: hidden;
-  border-radius: 50%;
-
-  img {
-    width: 100%;
-  }
-`;
-
-export const MessageWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  white-space: normal !important;
-
-  /* font-family: "Roboto", sans-serif; */
-  font-family: "BebasNeue";
-
-  color: white;
-  margin-bottom: 50px;
-  text-shadow: 1px 1px #000;
-`;
-
-export const MessageName = styled.div`
-  font-size: 2rem;
-  color: #fff;
-`;
-
-export const MessageText = styled.div`
-  font-size: 1.75rem;
-  word-break: break-word;
-
-  img {
-    height: 1rem;
-  }
 `;
