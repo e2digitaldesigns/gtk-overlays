22a80cc546847036b4bc88708c8bdae26a87d74d
Casana E. Bey <info@expresslayouts.com>
2024-05-31 13:07:57 -0400

    Update video

--- src/stories/globalComponents/VideoPlayer/VideoPlayer.stories.tsx
+++ src/stories/globalComponents/VideoPlayer/VideoPlayer.stories.tsx
@@ -13,9 +13,7 @@ const socket = mockSocket();
 const meta = {
   title: "Global Components/Video Player",
   component: Component,
-  parameters: {
-    layout: "centered"
-  },
+
   tags: ["autodocs"],
   argTypes: {
     defaultSize: {
@@ -29,48 +27,50 @@ const meta = {
         <>
           <div
             style={{
-              display: "grid",
-              gridTemplateColumns: "330px 500px",
-              background: "#000000",
-              width: "830px",
-              height: "345px"
+              display: "table",
+              width: "980px",
+              height: "560px",
+              background: "#111"
             }}
           >
             <div
               style={{
-                display: "grid",
-                gridTemplateColumns: "repeat(2, 1fr)",
-                gap: "5px",
-
-                position: "relative",
-                width: "320px",
-                padding: "5px"
-              }}
-            >
-              {Object.keys(VideoAction).map((key, index) => (
-                <Button
-                  key={index}
-                  onClick={() =>
-                    socket.triggerEvent("gtkOverlayVideoPlayer", {
-                      uid: "storybook",
-                      tid: "storybook",
-                      action: VideoAction[key as keyof typeof VideoAction]
-                    })
-                  }
-                >
-                  {key}
-                </Button>
-              ))}
-            </div>
-            <div
-              style={{
-                position: "relative",
-                width: "500px",
-                height: "290px"
+                margin: "5px",
+                width: "970px",
+                height: "550px",
+                background: "#31aafd",
+                position: "relative"
               }}
             >
               <Story />
             </div>
+          </div>
+
+          <div
+            style={{
+              margin: 0,
+              width: "980px",
+              background: "#111",
+              display: "grid",
+              gridTemplateColumns: "repeat(5, 1fr)",
+              gap: "5px",
+              padding: "5px"
+            }}
+          >
+            {Object.keys(VideoAction).map((key, index) => (
+              <Button
+                key={index}
+                onClick={() =>
+                  socket.triggerEvent("gtkOverlayVideoPlayer", {
+                    uid: "storybook",
+                    tid: "storybook",
+                    action: VideoAction[key as keyof typeof VideoAction]
+                  })
+                }
+              >
+                {key}
+              </Button>
+            ))}
           </div>
         </>
       );
@@ -85,22 +85,22 @@ export const Primary: Story = {
   args: {
     defaultSize: VideoSize.NORMAL,
     dimensions: {
-      top: "10px",
-      left: "10px",
-      width: "480px",
-      height: "270px"
+      top: "77.5px",
+      left: "120px",
+      width: "720px",
+      height: "405px"
     },
     smallScreenDimensions: {
-      top: "34px",
-      left: "60px",
-      width: "360px",
-      height: "203px"
+      top: "137.5px",
+      left: "242.5px",
+      width: "485px",
+      height: "275px"
     },
     fullScreenDimensions: {
-      top: "0px",
-      left: "0px",
-      width: "500px",
-      height: "290px"
+      top: "5px",
+      left: "5px",
+      width: "960px",
+      height: "540px"
     },
     allowFullScreen: true,
     allowSmallScreen: true,
