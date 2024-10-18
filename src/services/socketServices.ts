401e0dcd56e670898946bc65d9ab432b6452129b
Casana E. Bey <info@expresslayouts.com>
2024-10-18 19:39:56 -0400

    Remove sockets, chat relay and ranks

--- src/services/socketServices.ts
+++ src/services/socketServices.ts
@@ -1,23 +1,13 @@
 import io from "socket.io-client";
-import {
-  ChatMessgeReturn,
-  ChatRanker,
-  ChatRelayData,
-  ChatVoteData,
-  IEmojiReturn,
-  IVotes,
-  SocketServicesData
-} from "../types";
+import { ChatMessgeReturn, ChatVoteData, IEmojiReturn, IVotes, SocketServicesData } from "../types";
 
 const socket = io(process.env.REACT_APP_PUSH_SERVICE || "");
 
 enum SocketServicesEvents {
   GTK_APPLICATION_ACTION = "gtkApplicationAction",
   GTK_CHAT_DISPLAY = "gtkChatDisplay",
-  GTK_CHAT_RELAY = "gtkChatRelay",
   GTK_CHAT_VOTE = "gtkChatVote",
   GTK_OVERLAY_ACTION = "gtkOverlayAction",
-  GTK_OVERLAY_CHAT_RANKS = "gtkOverlayChatRanks",
   GTK_OVERLAY_EMOJIS = "gtkOverlayEmojis",
   GTK_OVERLAY_VIDEO_PLAYER = "gtkOverlayVideoPlayer",
   GTK_VOTING = "gtkVoting"
@@ -76,14 +66,6 @@ const socketServices = {
     socket?.removeAllListeners(SocketServicesEvents.GTK_OVERLAY_EMOJIS);
   },
 
-  subscribeOverlaysChatRanks(cb: (err: unknown, data: ChatRanker) => void) {
-    socket?.on(SocketServicesEvents.GTK_OVERLAY_CHAT_RANKS, (data: ChatRanker) => cb(null, data));
-  },
-
-  unSubscribeOverlaysChatRanks() {
-    socket?.removeAllListeners(SocketServicesEvents.GTK_OVERLAY_CHAT_RANKS);
-  },
-
   subscribeOverlaysVideoPlayer(cb: (err: unknown, data: SocketServicesData) => void) {
     socket?.on(SocketServicesEvents.GTK_OVERLAY_VIDEO_PLAYER, (data: SocketServicesData) =>
       cb(null, data)
@@ -92,14 +74,6 @@ const socketServices = {
 
   unSubscribeOverlaysVideoPlayer() {
     socket?.removeAllListeners(SocketServicesEvents.GTK_OVERLAY_VIDEO_PLAYER);
-  },
-
-  subscribeChatRelay(cb: (err: unknown, data: ChatRelayData) => void) {
-    socket?.on(SocketServicesEvents.GTK_CHAT_RELAY, (data: ChatRelayData) => cb(null, data));
-  },
-
-  unSubscribeChatRelay() {
-    socket?.removeAllListeners(SocketServicesEvents.GTK_CHAT_RELAY);
   }
 };
 
